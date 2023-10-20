1459fd939ef461a0313e3d40cfa204e1115248c7
VAIBHAV SUMAN <76209891+vsuman00@users.noreply.github.com>
2023-10-20 18:15:23 +0000

    Data Types Complete

---
+++ 01_basic/dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodeJS, not browser
+
+let name = "hitesh" // string
+let age = 18 // integer
+let isLoggedIn = false // boolean
+let state; // undefined
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
